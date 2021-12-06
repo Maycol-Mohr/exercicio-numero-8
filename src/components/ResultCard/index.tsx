3e54d0a4a038c9da77a045a08308aeb974c44e54
Maycol Mohr <maycolmohr@yahoo.com.br>
2021-12-06 21:41:37 +0100

    exercicio em andamento

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -1,16 +1,16 @@
 import './styles.css';
 
-
 type Props = {
  title?: string;
  description?: string;
  url?: string;
 }
+
 const ResultCard = ( { title, description, url} : Props) => {
 
     return (
         <div className="result-container">
-        <img src={url} />
+         <img src={url} />   
         <h3 className="result-title">{title}</h3>
         <p className="result-description">{description}</p>
         </div>
