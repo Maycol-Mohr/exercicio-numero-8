ef9130eee95a0f06b9d310682f5790ef84660efe
Maycol Mohr <maycolmohr@yahoo.com.br>
2021-12-22 19:45:28 +0100

    trabalho finalizado

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -22,22 +22,25 @@ const ResultCard = ({
   return (
     <div className="result-container">
       <div className="img-container">
-        <img src={url} alt={title} />{' '}
+        <img src={url} alt={title} />
       </div>
-      <div>
-        <h3 className="result-title">{title}</h3>
-        <p className="result-description">{description}</p>
-      </div>
-      <div>
+
+      <div className="info-container">
         <h2 className="titulo1">Informações</h2>
-        <h3 className="titulo2">Perfil:</h3>
-        <p>{html_url}</p>
-        <h3 className="titulo2">Seguidores:</h3>
-        <p>{followers}</p>
-        <h3 className="titulo2">Localidade:</h3>
-        <p>{location}</p>
-        <h3 className="titulo2">Nome:</h3>
-        <p>{name}</p>
+        <h3 className="titulo2">
+          Perfil: <a href={html_url}>{html_url}</a>
+        </h3>
+
+        <h3 className="titulo2">
+          Seguidores: <span className="titulo2-fonte-regular">{followers}</span>
+        </h3>
+
+        <h3 className="titulo2">
+          Localidade: <span className="titulo2-fonte-regular">{location}</span>{' '}
+        </h3>
+        <h3 className="titulo2">
+          Nome: <span className="titulo2-fonte-regular">{name}</span>{' '}
+        </h3>
       </div>
     </div>
   );
